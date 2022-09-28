294da1de6eb25dd5100fe198dcf759e3ec77fa74
Aldren Martin <113560992+AldrenMartin@users.noreply.github.com>
2022-09-28 02:54:53 +0000

    made ir read and display

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    
+  Scanner in = new Scanner(System.in);
+    
     System.out.println("Enter a word:");
+    String word = in.nextLine();
+
+    System.out.println(word.length());
+    
+
+
     
     //Your code will return the length of the entered String
   }
